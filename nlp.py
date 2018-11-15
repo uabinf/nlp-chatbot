f0fdc274cabbbbea705e4ca959234a1ae6f813b9
Andrew C Rios <arios29c@c0058.cm.cluster>
2018-11-15 00:36:53 -0600

    Train and pickle POS tagger

--- nlp.py
+++ nlp.py
@@ -1,7 +1,14 @@
-import nltk, os.path, requests, sys, tarfile
+import nltk, os.path, pickle, re, requests, sys, tarfile
 
+# Global settings
 tagger = None
 difficulty = 0
+
+# 
+determiners = ('das', 'dem', 'den', 'der', 'des', 'die', 'ein', 'eine', 'einem', 'einer')
+pronouns = ('deiner', 'dich', 'dir', 'du', 'er', 'es', 'euch', 'euer', 'ich', 'ihm', 'ihn', 'ihnen', 'ihr', 'ihrer', 'meiner', 'mich', 'mir', 'seiner', 'sie', 'uns', 'unser', 'wir')
+regex_digit = re.compile('\d')
+regex_punct = re.compile('[^\w\d]')
 
 def set_difficulty(d):
 	difficulty = d
@@ -39,47 +46,94 @@ def word_features(sent, i):
 		features['%+d:all_lower' % wr] = word.islower()
 		
 		# POS-specific features
+		features['%+d:determiner' % wr] = word.lower() in determiners
+		features['%+d:pronoun' % wr] = word.lower() in pronouns
 		features['%+d:digit' % wr] = bool(regex_digit.match(word))
 		features['%+d:punct' % wr] = bool(regex_punct.match(word))
-		
 	return features
 
+def get_featureset(dataset):
+	featureset = []
+	for sent in dataset:
+		for i in range(0, len(sent)):
+			featureset.append((word_features(sent, i), sent[i][1]))
+	return featureset
+
+def sent_features(sent):
+	return [word_features(sent, i) for i in range(len(sent))]
+
+
+def get_tagset(dataset):
+	tagset = set()
+	for sent in dataset:
+		for word in sent:
+			tagset.add(word[1])
+	return tagset
+
+def test_tagger(features):
+	global tagger
+
+	# Split into 90:10 training and testing corpus
+	cutoff = int(len(features) * 0.1)
+	trainset, testset = features[cutoff:], features[:cutoff]
+
+	# Train tagger and perform cross-fold validation
+	folds = 1
+	size = int(len(trainset) / folds)
+	accuracy = []
+	print('Performing %d-fold validation on tagger...' % folds)
+	for i in range(folds):
+		start = i * size
+		end = (i + 1) * size
+
+		subtrain = trainset[0:start] + trainset[end:-1]
+		subtest = testset[start:end]
+
+		tagger = nltk.classify.NaiveBayesClassifier.train(trainset)
+		accuracy.append(nltk.classify.accuracy(tagger, testset) * 100)
+	print('Average classification accuracy:', sum(accuracy) / folds)
+
 def init_tagger():
+	global tagger
+
 	if not tagger:
-		download_corpus()
-		print('Reading corpus sentences...')
-		corpus = nltk.corpus.ConllCorpusReader('.', 'tiger_release_aug07.corrected.16012013.conll09', ['ignore', 'words', 'ignore', 'ignore', 'pos'], encoding='utf-8')
-		tagged_sents = corpus.tagged_sents()
+		if not os.path.isfile('tagger.pickle'):
+			# Download and retrieve corpus sentences
+			download_corpus()
+			print('Reading corpus sentences...')
+			corpus = nltk.corpus.ConllCorpusReader('.', 'tiger_release_aug07.corrected.16012013.conll09', ['ignore', 'words', 'ignore', 'ignore', 'pos'], encoding='utf-8')
 
-		# Split into 90:10 training and testing corpus
-		cutoff = int(len(tagged_sents) * 0.1)
-		trainset, testset = tagged_sents[cutoff:], tagged_sents[:cutoff]
+			# Train tagger on corpus
+			dataset = corpus.tagged_sents()
+			features = get_featureset(dataset)
+			tagger = nltk.classify.NaiveBayesClassifier.train(features)
 
-		# Retrieve tagset
-		print('Extracting tagset...')
-		tags = set()
-		for sent in tagged_sents:
-			for word in sent:
-				tags.add(word[1])
-		print(tags)
-
-		# Train tagger
-		# ...
+			# Pickle tagger to prevent retraining during each startup
+			print('Storing tagger for reuse...')
+			with open('tagger.pickle', 'wb') as tagfile:
+				pickle.dump(tagger, tagfile, protocol=pickle.HIGHEST_PROTOCOL)
+		else:
+			# Load previously-trained tagger from file
+			print('Loading previously-trained tagger...')
+			with open('tagger.pickle', 'rb') as tagfile:
+				tagger = pickle.load(tagfile)
 
 def format_error(word, chance, message):
 	return 'Mistake (%s%% certainty) at word \'%s\': %s' % (chance, word, message)
 
 def rate_sentence(s):
-	init_tagger()
+	global tagger
 
+	# Initialize tagger, get sentence tokens
+	init_tagger()
 	tokens = nltk.word_tokenize(s)
 	feedback = []
 	total = len(tokens)
 	correct = total
 
 	# Perform a bunch of checks on sentence, add errors like so
-	feedback.append(format_error('he', 0.95, 'Expected possessive pronoun.'))
-	feedback.append(format_error('donald', 0.99, 'Expected capitalization at start of sentence.'))
+	# feedback.append(format_error('he', 0.95, 'Expected possessive pronoun.'))
+	# feedback.append(format_error('donald', 0.99, 'Expected capitalization at start of sentence.'))
 
 	if not feedback:
 		feedback.append('No errors were found!')
